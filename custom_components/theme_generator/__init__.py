e71c533ec999cbaa9aa9e710018847974097abbf
Piranha1605 <66534318+Piranha1605@users.noreply.github.com>
2026-06-30 14:42:53 +0000

    Release v1.3.2 add HACS brand icon

--- custom_components/theme_generator/__init__.py
+++ custom_components/theme_generator/__init__.py
@@ -44,7 +44,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         config={
             "_panel_custom": {
                 "name": PANEL_TAG,
-                "module_url": f"/{DOMAIN}_static/{PANEL_FILENAME}?v=1.3.1",
+                "module_url": f"/{DOMAIN}_static/{PANEL_FILENAME}?v=1.3.2",
                 "embed_iframe": False,
                 "trust_external": False,
             }
