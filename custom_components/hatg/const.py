741ad431d9a63efdd243ffe2385ca321227d81ac
Piranha1605 <66534318+Piranha1605@users.noreply.github.com>
2026-07-29 10:52:50 +0000

    v0.2.36: Neue Seite Cardmod-Vorlagen - 6 Bausteine (Icon-Farbe/-Hintergrund, Kartenfarben-Verlauf, Glow/Schatten, Rahmen-Akzent, Glas-Effekt, Relief) per Klick global im Theme aktivierbar, markiertes Block-System in card-mod-root-yaml

--- custom_components/hatg/const.py
+++ custom_components/hatg/const.py
@@ -1,13 +1,13 @@
 DOMAIN = "hatg"
 NAME = "HATG"
-VERSION = "0.2.35"
+VERSION = "0.2.36"
 
 PANEL_URL = "hatg"
 PANEL_TITLE = "HATG"
 PANEL_ICON = "mdi:palette-swatch"
 PANEL_TAG = "hatg-panel"
 STATIC_PATH = "/hatg_static"
-FRONTEND_MODULE = "/hatg_static/hatg-panel.js?v=0.2.35"
+FRONTEND_MODULE = "/hatg_static/hatg-panel.js?v=0.2.36"
 
 # v0.2.20: Hintergrund-Kachel "Bild" - eigener Web-Pfad fuer config/themes/Wallpaper/,
 # damit vom Nutzer hochgeladene Bilder trotz HAs sonst fehlender Freigabe von
