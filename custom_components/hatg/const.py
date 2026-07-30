cf7fd61baaa9dd100acc91dec1c254c30b88b918
Piranha1605 <66534318+Piranha1605@users.noreply.github.com>
2026-07-30 18:07:37 +0000

    v0.3.11: Vorschaubilder der Wallpaper-Galerie blieben leer, weil die Bild-URL das style-Attribut vorzeitig beendet hat

--- custom_components/hatg/const.py
+++ custom_components/hatg/const.py
@@ -1,12 +1,12 @@
 DOMAIN = "hatg"
 NAME = "HATG"
-VERSION = "0.3.10"
+VERSION = "0.3.11"
 
 PANEL_URL = "hatg"
 PANEL_TITLE = "HATG"
 PANEL_ICON = "mdi:palette-swatch"
 PANEL_TAG = "hatg-panel"
 STATIC_PATH = "/hatg_static"
-FRONTEND_MODULE = "/hatg_static/hatg-panel.js?v=0.3.10"
+FRONTEND_MODULE = "/hatg_static/hatg-panel.js?v=0.3.11"
 
 WALLPAPER_STATIC_PATH = "/hatg_wallpaper"
